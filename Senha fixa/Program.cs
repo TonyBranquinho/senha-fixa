b9ed6ba5a8660521d7f3f034fd438d3dfa87a838
Tony <antony.branquinho@gmail.com>
2023-12-04 15:54:50 -0300

    bem facil

---
+++ Senha fixa/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Senha_fixa {
+    class Program {
+        static void Main(string[] args) {
+
+            int numero, senha;
+
+            Console.Write("Digite a senha: ");
+            numero = int.Parse(Console.ReadLine());
+
+            senha = 2002;
+
+            while (numero != senha) {
+                Console.Write("Senha invalida. Tente novamente: ");
+                numero = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Acesso permitido.");
+        }
+    }
+}
